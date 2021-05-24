344b7222aae4647961efca7121d3ea8586033f18
Jonathan Gonzalez <Jonathan.gonzalez@powerhrg.com>
2021-05-23 21:27:40 -0400

    added index.js update

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers){
+    return drivers.slice(0,2)
+}
+
+function returnLastTwoDrivers(drivers){
+    return drivers.slice(drivers.length-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(fareQuintupler){
+    return function(){
+        return fareQuintupler * 5
+    }
+}
+
+function fareDoubler(faredoubler){
+    return faredoubler * 2
+}
+
+function fareTripler(fareTripler){
+    return fareTripler * 3
+}
+
+function selectDifferentDrivers(drivers, name){
+    return name(drivers)
+}
